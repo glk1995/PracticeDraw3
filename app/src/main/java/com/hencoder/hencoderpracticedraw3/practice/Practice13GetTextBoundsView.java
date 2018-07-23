9a3e9d4ec0c81947e077e0ebca8b15d83952cdaf
glk <77833601@qq.com>
2018-07-23 15:07:48 +0800

    练习

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -19,6 +20,7 @@ public class Practice13GetTextBoundsView extends View {
     String text6 = "â";
     int top = 200;
     int bottom = 400;
+    int[] yOffset = {0, 0, 0, 0, 0, 0};
 
     public Practice13GetTextBoundsView(Context context) {
         super(context);
@@ -37,6 +39,25 @@ public class Practice13GetTextBoundsView extends View {
         paint1.setStrokeWidth(20);
         paint1.setColor(Color.parseColor("#E91E63"));
         paint2.setTextSize(160);
+
+        Rect bound = new Rect();
+        paint2.getTextBounds(text1, 0, text1.length(), bound);
+        //这里我top应该是基于baseline的top，所以top为负数，bottom为正数
+        //这里相加除以2得到基于baseline的中点
+        //举例说一个字绘制rect -50 ~ 50，一个字正常绘制是top为0，bottom 50
+        //那么如果绘制在中间，必须往上移动(0 + 50) / 2
+        //相当于getTextBounds获取的是这个字原来基准线的top、bottom，现在是矩形的中心对齐，基准线改为矩形的middle
+        yOffset[0] = -(bound.top + bound.bottom) / 2;
+        paint2.getTextBounds(text2, 0, text2.length(), bound);
+        yOffset[1] = -(bound.top + bound.bottom) / 2;
+        paint2.getTextBounds(text3, 0, text3.length(), bound);
+        yOffset[2] = -(bound.top + bound.bottom) / 2;
+        paint2.getTextBounds(text4, 0, text4.length(), bound);
+        yOffset[3] = -(bound.top + bound.bottom) / 2;
+        paint2.getTextBounds(text5, 0, text5.length(), bound);
+        yOffset[4] = -(bound.top + bound.bottom) / 2;
+        paint2.getTextBounds(text6, 0, text5.length(), bound);
+        yOffset[5] = -(bound.top + bound.bottom) / 2;
     }
 
     @Override
@@ -50,11 +71,11 @@ public class Practice13GetTextBoundsView extends View {
         // 这种居中算法的优点是，可以让文字精准地居中，分毫不差
 
         int middle = (top + bottom) / 2;
-        canvas.drawText(text1, 100, middle, paint2);
-        canvas.drawText(text2, 200, middle, paint2);
-        canvas.drawText(text3, 300, middle, paint2);
-        canvas.drawText(text4, 400, middle, paint2);
-        canvas.drawText(text5, 500, middle, paint2);
-        canvas.drawText(text6, 600, middle, paint2);
+        canvas.drawText(text1, 100, middle + yOffset[0], paint2);
+        canvas.drawText(text2, 200, middle + yOffset[1], paint2);
+        canvas.drawText(text3, 300, middle + yOffset[2], paint2);
+        canvas.drawText(text4, 400, middle + yOffset[3], paint2);
+        canvas.drawText(text5, 500, middle + yOffset[4], paint2);
+        canvas.drawText(text6, 600, middle + yOffset[5], paint2);
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice14GetFontMetricsView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice14GetFontMetricsView.java
@@ -14,6 +14,7 @@ public class Practice14GetFontMetricsView extends View {
     String[] texts = {"A", "a", "J", "j", "Â", "â"};
     int top = 200;
     int bottom = 400;
+    float yOffset;
 
     public Practice14GetFontMetricsView(Context context) {
         super(context);
@@ -32,6 +33,15 @@ public class Practice14GetFontMetricsView extends View {
         paint1.setStrokeWidth(20);
         paint1.setColor(Color.parseColor("#E91E63"));
         paint2.setTextSize(160);
+
+        //获取文字排版的几个数值
+        //Paint.FontMetrics fontMetrics = paint2.getFontMetrics();
+        //ascent、descent 普通文字的绘制的顶部和底部
+        //top、bottom 所有文字绘制的顶部和底部
+        //leading 上一行文字的底部和下一行文字顶部距离
+        //yOffset = -(fontMetrics.descent + fontMetrics.ascent) / 2;
+        //简易方法
+        yOffset = - (paint2.ascent() + paint2.descent()) / 2;
     }
 
     @Override
@@ -45,11 +55,11 @@ public class Practice14GetFontMetricsView extends View {
         // 这种居中算法的优点是，可以让不同的文字的 baseline 对齐
 
         int middle = (top + bottom) / 2;
-        canvas.drawText(texts[0], 100, middle, paint2);
-        canvas.drawText(texts[1], 200, middle, paint2);
-        canvas.drawText(texts[2], 300, middle, paint2);
-        canvas.drawText(texts[3], 400, middle, paint2);
-        canvas.drawText(texts[4], 500, middle, paint2);
-        canvas.drawText(texts[5], 600, middle, paint2);
+        canvas.drawText(texts[0], 100, middle + yOffset, paint2);
+        canvas.drawText(texts[1], 200, middle + yOffset, paint2);
+        canvas.drawText(texts[2], 300, middle + yOffset, paint2);
+        canvas.drawText(texts[3], 400, middle + yOffset, paint2);
+        canvas.drawText(texts[4], 500, middle + yOffset, paint2);
+        canvas.drawText(texts[5], 600, middle + yOffset, paint2);
     }
 }
